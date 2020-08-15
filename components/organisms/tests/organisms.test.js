6b2e7256f5b4f4b24856ab3333f249a75e4dcd28
taka <tomo05061@gmail.com>
2020-08-16 00:44:50 +0900

    BaseNavのテスト処理を修正

--- components/organisms/tests/organisms.test.js
+++ components/organisms/tests/organisms.test.js
@@ -3,18 +3,28 @@ import BaseNav from '../BaseNav.vue';
 
 // NavIcon
 describe('BaseNav', () => {
+  const dummyLogo = 'ダミー';
   const baseNav = mount(BaseNav, {
     stubs: {
       NuxtLink: RouterLinkStub,
     },
     propsData: {
       isOpen: true,
+      logoText: dummyLogo,
     },
+  });
+
+  it('開いた時のスナップショット', () => {
+    expect(baseNav.html()).toMatchSnapshot();
   });
 
   it('changeStateのテスト', () => {
     expect(baseNav.vm.$data.isOpen).toBe(false);
     baseNav.vm.changeState();
     expect(baseNav.vm.$data.isOpen).toBe(true);
+  });
+
+  it('閉じた時のスナップショット', () => {
+    expect(baseNav.html()).toMatchSnapshot();
   });
 });
